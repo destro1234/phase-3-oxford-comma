dc42e727d695b32fb75b5981735edf3a8d95eb10
Elias <eliasventura23@gmail.com>
2022-10-19 18:43:40 -0400

    finished lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length >= 3
+        last_element = array.pop
+        [array.join(', '), last_element].join(', and ')
+    else
+        array.join(" , ")
+    end
+        
 end
